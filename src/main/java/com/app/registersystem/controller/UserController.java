9340ed18554f23bfbfd3c3e3f5a10cc899550a70
suprajapalagiri <supraja.palagiri@ojas-it.com>
2021-03-16 12:34:46 +0530

    controller changes

--- src/main/java/com/app/registersystem/controller/UserController.java
+++ src/main/java/com/app/registersystem/controller/UserController.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
+import org.aspectj.apache.bcel.classfile.Code;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,12 +25,26 @@ import com.app.registersystem.model.User;
 import com.app.registersystem.service.UserService;
 import com.app.registersystem.validation.DtoValidationUtils;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(value="User controller",description = "Rest Api related to User entity!!")
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	@Autowired
 	private UserService userService;
 
+	
+	@ApiOperation(value="adding person into records")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200,message = "Success"),
+			@ApiResponse(code = 401, message = "not authorized!"),
+			@ApiResponse(code = 403, message = "forbidden!!!"),
+			@ApiResponse(code = 404, message = "not found!!!")
+	})
 	@PostMapping("/createUser")
 	public ResponseEntity<?> saveUser(@RequestBody UserDTO userDTO) throws URISyntaxException {
 		DtoValidationUtils.validateUser(userDTO);
@@ -40,6 +55,15 @@ public class UserController {
 		return ResponseEntity.created(new URI("user/createUser")).body(UserTransformer.fromEntityToDTO(saveUser));
 	}
 
+	@ApiOperation(value = "view a list of persons based on date" )
+	@ApiResponses(value = {
+			@ApiResponse(code = 200,message = "Success"),
+			@ApiResponse(code = 401, message = "not authorized!"),
+			@ApiResponse(code = 403, message = "forbidden!!!"),
+			@ApiResponse(code = 404, message = "not found!!!")
+	})
+	
+	
 	@GetMapping("/getByDate/{date}")
 	public ResponseEntity<List<User>> getByDate(@PathVariable String date) throws ParseException {
 
